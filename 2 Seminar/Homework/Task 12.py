a1abbadb088894c94d7d541956be5feb2cffc29a
AnnaKiryukhina <124432443+AnnaKiryukhina@users.noreply.github.com>
2023-04-18 19:44:05 +0700

    семинар 3 задача 1.

--- 2 Seminar/Homework/Task 12.py
+++ 2 Seminar/Homework/Task 12.py
@@ -10,3 +10,10 @@ x = (P + math.sqrt (D))/2
 y = P - x
 print(x)
 print(y)
+
+# x = int(input())
+# y = int(input())
+# for i in range(x):
+#     for j in range(y):
+#         if x == i + j and y == i * j:
+# print(i, j)
